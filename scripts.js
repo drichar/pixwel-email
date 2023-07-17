0c373b5207c7cde29ce10b4d668f52c250a90d22
Doug Richar <drichar@gmail.com>
2023-07-17 10:33:36 -0400

    feat: add repository and GitHub Pages links

--- scripts.js
+++ scripts.js
@@ -1,9 +1,7 @@
 const templates = [
   {
     name: 'Basic / Welcome',
-    mjml: 'templates/basic.mjml',
-    output: 'output/basic.html',
-    preview: '#'
+    slug: 'basic'
   }
 ]
 
@@ -16,23 +14,23 @@ templates.forEach((template) => {
     <td class="flex items-center flex-wrap w-full py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:pl-0 lg:text-base">
 			<span class="py-1 mr-4 sm:m-0">${template.name}</span>
 			<div class="sm:hidden flex items-center space-x-2 py-1">
-				<a href="${template.mjml}" class="rounded bg-white px-2 py-1 text-sm font-medium text-indigo-600 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+				<a href="https://github.com/drichar/pixwel-email/blob/main/templates/${template.slug}.mjml" class="rounded bg-white px-2 py-1 text-sm font-medium text-indigo-600 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
 					MJML
 				</a>
-				<a href="${template.output}" class="rounded bg-white px-2 py-1 text-sm font-medium text-indigo-600 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+				<a href="https://github.com/drichar/pixwel-email/blob/main/output/${template.slug}.html" class="rounded bg-white px-2 py-1 text-sm font-medium text-indigo-600 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
 					Output
 				</a>
 			</div>
 		</td>
 		<td class="hidden px-3 py-4 text-sm font-medium text-gray-500 sm:table-cell lg:text-base">
-			<a href="${template.mjml}" class="text-indigo-600 hover:text-indigo-900">${template.mjml}</a>
+			<a href="https://github.com/drichar/pixwel-email/blob/main/templates/${template.slug}.mjml" class="text-indigo-600 hover:text-indigo-900">templates/${template.slug}.mjml</a>
 		</td>
 		<td class="hidden px-3 py-4 text-sm font-medium text-gray-500 sm:table-cell lg:text-base">
-			<a href="${template.output}" class="text-indigo-600 hover:text-indigo-900">${template.output}</a>
+			<a href="https://github.com/drichar/pixwel-email/blob/main/output/${template.slug}.html" class="text-indigo-600 hover:text-indigo-900">output/${template.slug}.html</a>
 		</td>
 		<td class="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0 lg:text-base">
-			<a href="${template.preview}" class="inline-flex rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-				Preview<span class="sr-only">, Basic / Welcome</span>
+			<a href="output/${template.slug}.html" class="inline-flex rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+				Preview<span class="sr-only">, ${template.name}</span>
 			</button>
 		</td>
   `
